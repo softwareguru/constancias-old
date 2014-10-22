efd238fe75ec2ea5c73854f55fac285f2bec6e29
Pedro Galvan <pedro@sg.com.mx>
2014-10-21 22:26:07 -0500

    Constantes default

---
+++ constants.php
@@ -0,0 +1,17 @@
+<?php 
+
+// Defines constants for use in generar-constancias.php
+
+define ("RESULTSDIR", "/var/www/constancias/results");
+define ("BASEURL", "http://tuserver");
+define ("DBHOST", "localhost");
+define ("DBNAME", "constancias");
+define ("DBUSER", "constancias");
+define ("DBPASS", "constancias");
+define ("SENDEREMAIL", "remitente@tudominio.com");
+define ("SENDERNAME", "Nombre Remitente");
+define ("SMTPHOST", "smtpserver");
+define ("SMTPUSER", "smtpuser");
+define ("SMTPPASS", "smtppassword");
+
+?>
